ee50279a478e397c2eb835ebba7dfb36886aa6f6
Kamilla Kiss <kamilla0420@gmail.com>
2020-04-14 13:00:52 +0200

    Next level shit

--- dogshelter_web/dogshelter_web/adoptable_dogs/admin.py
+++ dogshelter_web/dogshelter_web/adoptable_dogs/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Dog, DogImages
 
-admin.site.register(Dog)
-admin.site.register(DogImages)
+class DogImagesInline(admin.TabularInline):
+    model = DogImages
+
+class DogAdmin(admin.ModelAdmin):
+    inlines = [
+        DogImagesInline,
+    ]
+
+admin.site.register(Dog, DogAdmin)
--- dogshelter_web/dogshelter_web/general/models.py
+++ dogshelter_web/dogshelter_web/general/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Gallery(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, blank=True)
     pub_date = models.DateField()
     image = models.ImageField(upload_to='images/')
     publisher = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def pub_date_pretty(self):
-        return self.pub_date.strftime('%Y. %b. %e.')
+        return self.pub_date.strftime('%Y. %b.')
 
 class News(models.Model):
     title = models.CharField(max_length=100)
